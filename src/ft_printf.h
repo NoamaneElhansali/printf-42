f734f4452e1923e8a42cd42a38c4b9ea52b9c0d7
Noamane Elhansali <nelhansa@e1r5p4.1337.ma>
2025-11-06 20:50:31 +0100

    change norminatte

--- src/ft_printf.h
+++ src/ft_printf.h
@@ -1,14 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nelhansa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/06 20:35:59 by nelhansa          #+#    #+#             */
+/*   Updated: 2025/11/06 20:36:45 by nelhansa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include <stdarg.h>
 # include <unistd.h>
-#include <stdarg.h>
-int ft_putchar(char c);
-int ft_putstr(char *s);
-int ft_putnbr(int n);
-int ft_printf(const char *format, ...);
-int ft_putptr(void *ptr);
-int ft_put_nbr_hex(unsigned long long num);
-int ft_putnbr_hex(int num,int upercase);
+
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_putnbr(int n);
+int	ft_printf(const char *format, ...);
+int	ft_putptr(void *ptr);
+int	ft_put_nbr_hex(unsigned long long num);
+int	ft_putnbr_hex(int num, int upercase);
 
 #endif
